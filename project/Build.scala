040bcc6680fe6f17678146f34c04bc8bad1b6e95
gundi <hock@zhaw.ch>
2012-11-23 20:44:17 +0100

    scalaz 7 M5

--- project/Build.scala
+++ project/Build.scala
@@ -77,10 +77,10 @@ object Dependencies {
   val nbAutoupdateServices = "org.netbeans.api" % "org-netbeans-modules-autoupdate-services" % nbV
   val nbModulesOptions = "org.netbeans.api" % "org-netbeans-modules-options-api" % nbV
 
-  val scalaz_core = "org.scalaz" %% "scalaz-core" % "7.0.0-M4"
-  val scalaz_effect = "org.scalaz" %% "scalaz-effect" % "7.0.0-M4"
+  val scalaz_core = "org.scalaz" %% "scalaz-core" % "7.0.0-M5"
+  val scalaz_effect = "org.scalaz" %% "scalaz-effect" % "7.0.0-M5"
   val scalaz_scalacheck =
-    "org.scalaz" %% "scalaz-scalacheck-binding" % "7.0.0-M4"
+    "org.scalaz" %% "scalaz-scalacheck-binding" % "7.0.0-M5"
   val scalaz_scalacheckT = scalaz_scalacheck % "test"
 
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.9"
