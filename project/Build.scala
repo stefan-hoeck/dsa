78db608fe4d5b5f595c30f73c49364d857a20f0a
gundi <hock@zhaw.ch>
2013-02-24 20:57:36 +0100

    back to pure Netbeans module setup

--- project/Build.scala
+++ project/Build.scala
@@ -2,18 +2,17 @@ import sbt._
 import Keys._
 
 object BuildSettings {
-  import Resolvers._
-
   val sv = "2.10.0"
   val buildOrganization = "efa.dsa"
   val buildVersion = "1.0.0-SNAPSHOT"
   val buildScalaVersion = sv
+  val netbeansRepo = "Netbeans" at "http://bits.netbeans.org/maven2/"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
     version := buildVersion,
     scalaVersion := buildScalaVersion,
-    resolvers ++= repos,
+    resolvers += netbeansRepo,
     scalacOptions ++= Seq ("-deprecation", "-feature", "-language:higherKinds",
       "-language:postfixOps"),
     publishArtifact in (Compile, packageDoc) := false,
@@ -21,84 +20,78 @@ object BuildSettings {
   )
 } 
 
-object Resolvers {
- val netbeansRepo = "Netbeans" at "http://bits.netbeans.org/maven2/"
- val scalatoolsRepo = "Scala-Tools Maven2 Repository Releases" at
-   "http://scala-tools.org/repo-releases"
- val sonatypeRepo = "releases" at
-   "http://oss.sonatype.org/content/repositories/releases"
-
- val repos = Seq (netbeansRepo, scalatoolsRepo, sonatypeRepo)
-}
-
 object Dependencies {
   import BuildSettings.sv  
 
-  val utilVersion = "0.2.1-SNAPSHOT"
-  val reactVersion = "0.2.1-SNAPSHOT"
+  val utilV = "0.2.1-SNAPSHOT"
+  val reactV = "0.2.1-SNAPSHOT"
+  val efaNbV = "0.2.1-SNAPSHOT"
+  val rpgV = "1.0.0-SNAPSHOT"
+  val nbV = "RELEASE71"
+  val scalazV = "7.0.0-M8"
+
   val util = "efa"
   val react = "efa.react"
-  val efaCore = util %% "efa-core" % utilVersion changing
-
-  val efaIo = util %% "efa-io" % utilVersion changing
-
-  val efaNb = util %% "efa-nb" % utilVersion changing
-
-  val efaReact = react %% "react-core" % reactVersion changing
-
-  val efaReactSwing = react %% "react-swing" % reactVersion changing
-
-  val rpgVersion = "1.0.0-SNAPSHOT"
   val rpg = "efa.rpg"
-  val rpgBeing = rpg %% "rpg-being" % rpgVersion changing
+  val nb = "org.netbeans.api"
+  val scalaz = "org.scalaz"
 
-  val rpgCore = rpg %% "rpg-core" % rpgVersion changing
+  val efaCore = util %% "efa-core" % utilV changing
 
-  val rpgItems = rpg %% "rpg-items" % rpgVersion changing
+  val efaIo = util %% "efa-io" % utilV changing
 
-  val rpgRules = rpg %% "rpg-rules" % rpgVersion changing
+  val efaNb = "efa.nb" %% "efa-nb" % efaNbV changing
 
-  val nbV = "RELEASE71"
+  val efaReact = react %% "react-core" % reactV changing
+
+  val efaReactSwing = react %% "react-swing" % reactV changing
+
+  val rpgBeing = rpg %% "rpg-being" % rpgV changing
+
+  val rpgCore = rpg %% "rpg-core" % rpgV changing
+
+  val rpgItems = rpg %% "rpg-items" % rpgV changing
+
+  val rpgRules = rpg %% "rpg-rules" % rpgV changing
 
   val scalaSwing = "org.scala-lang" % "scala-swing" % sv
  
-  val nbAnnotations = "org.netbeans.api" % "org-netbeans-api-annotations-common" % nbV
-  val nbUtil = "org.netbeans.api" % "org-openide-util" % nbV
-  val nbLookup = "org.netbeans.api" % "org-openide-util-lookup" % nbV
-  val nbExplorer = "org.netbeans.api" % "org-openide-explorer" % nbV
-  val nbWindows = "org.netbeans.api" % "org-openide-windows" % nbV
-  val nbNodes = "org.netbeans.api" % "org-openide-nodes" % nbV
-  val nbFilesystems = "org.netbeans.api" % "org-openide-filesystems" % nbV
-  val nbLoaders = "org.netbeans.api" % "org-openide-loaders" % nbV
-  val nbModules = "org.netbeans.api" % "org-openide-modules" % nbV
-  val nbAwt = "org.netbeans.api" % "org-openide-awt" % nbV
-  val nbSettings = "org.netbeans.api" % "org-netbeans-modules-settings" % nbV
-  val nbActions = "org.netbeans.api" % "org-openide-actions" % nbV
-  val nbDialogs = "org.netbeans.api" % "org-openide-dialogs" % nbV
-  val nbOutline = "org.netbeans.api" % "org-netbeans-swing-outline" % nbV
-  val nbAutoupdateUi = "org.netbeans.api" % "org-netbeans-modules-autoupdate-ui" % nbV
-  val nbAutoupdateServices = "org.netbeans.api" % "org-netbeans-modules-autoupdate-services" % nbV
-  val nbModulesOptions = "org.netbeans.api" % "org-netbeans-modules-options-api" % nbV
+  val nbAnnotations = nb % "org-netbeans-api-annotations-common" % nbV
+  val nbUtil = nb % "org-openide-util" % nbV
+  val nbLookup = nb % "org-openide-util-lookup" % nbV
+  val nbExplorer = nb % "org-openide-explorer" % nbV
+  val nbWindows = nb % "org-openide-windows" % nbV
+  val nbNodes = nb % "org-openide-nodes" % nbV
+  val nbFilesystems = nb % "org-openide-filesystems" % nbV
+  val nbLoaders = nb % "org-openide-loaders" % nbV
+  val nbModules = nb % "org-openide-modules" % nbV
+  val nbAwt = nb % "org-openide-awt" % nbV
+  val nbSettings = nb % "org-netbeans-modules-settings" % nbV
+  val nbActions = nb % "org-openide-actions" % nbV
+  val nbDialogs = nb % "org-openide-dialogs" % nbV
+  val nbOutline = nb % "org-netbeans-swing-outline" % nbV
+  val nbAutoupdateUi = nb % "org-netbeans-modules-autoupdate-ui" % nbV
+  val nbAutoupdateServices = nb % "org-netbeans-modules-autoupdate-services" % nbV
+  val nbModulesOptions = nb % "org-netbeans-modules-options-api" % nbV
 
-  val scalaz_core = "org.scalaz" %% "scalaz-core" % "7.0.0-M8"
-  val scalaz_effect = "org.scalaz" %% "scalaz-effect" % "7.0.0-M8"
-  val scalaz_scalacheck =
-    "org.scalaz" %% "scalaz-scalacheck-binding" % "7.0.0-M8"
-  val scalaz_scalacheckT = scalaz_scalacheck % "test"
+  val shapeless = "com.chuusai" %% "shapeless" % "1.2.3"
+  val scalaz_core = scalaz %% "scalaz-core" % scalazV
+  val scalaz_effect = scalaz %% "scalaz-effect" % scalazV
+  val scalaz_scalacheck = scalaz %% "scalaz-scalacheck-binding" % scalazV
 
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.10.0"
-  val scalacheckT = scalacheck % "test"
-  val scalazCheckT = Seq(scalaz_core, scalaz_scalacheckT, scalacheckT)
-  val scalazCheckET = scalazCheckT :+ scalaz_effect
+
+  val coolness = Seq(scalaz_core, scalaz_effect, scalaz_scalacheck,
+                     shapeless, scalacheck)
 }
 
 object DsaBuild extends Build {
-  import Resolvers._
   import Dependencies._
   import BuildSettings._
 
-  def addDeps (ds: Seq[ModuleID]) =
-    BuildSettings.buildSettings ++ Seq (libraryDependencies ++= ds)
+  def addDeps (ds: ModuleID*) =
+    BuildSettings.buildSettings ++ 
+    Seq(libraryDependencies ++= (ds ++ coolness))
 
   lazy val dsa = Project (
     "dsa",
@@ -112,77 +105,75 @@ object DsaBuild extends Build {
   lazy val abilities = Project (
     "dsa-abilities",
     file("abilities"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   ) dependsOn (world)
 
   lazy val abilitiesServices = Project (
     "dsa-abilitiesservices",
     file("abilitiesservices"),
-    settings = addDeps (scalazCheckET ++ Seq(efaCore, efaNb, rpgCore, rpgItems))
+    settings = addDeps(efaCore, efaNb, rpgCore, rpgItems)
   ) dependsOn (world, abilities, beingServices)
 
   lazy val being = Project (
     "dsa-being",
     file("being"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   ) dependsOn (world, abilities, equipment, generation)
 
   lazy val beingCalc = Project (
     "dsa-beingCalc",
     file("beingCalc"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   ) dependsOn (world, abilities, equipment, generation, being, testItems)
 
   lazy val beingServices = Project (
     "dsa-beingServices",
     file("beingServices"),
-    settings = addDeps (scalazCheckT ++
-      Seq(efaCore, efaReact, rpgCore, rpgBeing, rpgRules))
+    settings = addDeps(efaCore, efaReact, rpgCore, rpgBeing, rpgRules)
   ) dependsOn (being, beingCalc)
 
   lazy val beingUI = Project (
     "dsa-beingUI",
     file("beingUI"),
-    settings = addDeps (scalazCheckT ++
-      Seq(efaCore, efaNb, efaReact, efaReactSwing, rpgCore, rpgBeing))
+    settings = addDeps(efaCore, efaNb, efaReact, efaReactSwing, rpgCore, rpgBeing)
   ) dependsOn (beingServices)
 
   lazy val equipment = Project (
     "dsa-equipment",
     file("equipment"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   ) dependsOn (world)
 
   lazy val equipmentServices = Project (
     "dsa-equipmentservices",
     file("equipmentservices"),
-    settings = addDeps (scalazCheckET ++ Seq(efaCore, efaNb, rpgCore, rpgItems))
+    settings = addDeps(efaCore, efaNb, rpgCore, rpgItems)
   ) dependsOn (world, beingServices, equipment)
 
   lazy val generation = Project (
     "dsa-generation",
     file("generation"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   ) dependsOn (world)
 
   lazy val rules = Project (
     "dsa-rules",
     file("rules"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore, rpgRules))
+    settings = addDeps(efaCore, rpgCore, rpgRules)
   ) dependsOn (world, abilities, equipment, generation, being,
                beingCalc, testItems, beingServices)
 
   lazy val testItems = Project (
     "dsa-testItems",
     file("testItems"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   ) dependsOn (world, abilities, equipment, generation, being)
 
   lazy val world = Project (
     "dsa-world",
     file("world"),
-    settings = addDeps (scalazCheckT ++ Seq(efaCore, rpgCore))
+    settings = addDeps(efaCore, rpgCore)
   )
 }
 
-// vim: set ts=2 sw=2 et:
+// vim: set ts=2 sw=2 nowrap et:
